ce8f7464059447d6798d5a300a328dd9c7776cb8
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2017-08-07 11:53:41 -0700

    create helper files in e2e

---
+++ tests/e2e/helpers/db.js
@@ -0,0 +1,14 @@
+const connection = require('mongoose').connection;
+const request = require('./request');
+
+/* export a small helper for dropping the db*/
+module.exports = {
+    drop() {
+        return connection.dropDatabase();
+    },
+    getToken(user = { email: 'me@me.com', password: 'abc' }) {
+        return request.post('/auth/signup')
+            .send(user)
+            .then(res => res.body.token);
+    }
+};
