1d7bee9132ab8445b08e189ea9a0a03dbe5ac233
Anwar Montasir <anwarmontasir@gmail.com>
2017-08-07 14:14:23 -0700

    one working search test

--- tests/e2e/search.test.js
+++ tests/e2e/search.test.js
@@ -1,4 +1,20 @@
 //const db = require('./db');
-const request = require('./request');
+require('dotenv').config();
+const request = require('./_request');
 const assert = require('chai').assert;
 
+describe('search route', () => {
+
+    const artist = 'Nirvana';
+    const title = 'Nevermind';
+
+    it('searches an album', () => {
+        return request.get(`/search/artist=${artist}&release_title=${title}`)
+            .then(res => res.body)
+            .then(results => {
+                console.log("RESULTS = ", JSON.parse(results.text));
+                assert.ok(results);
+            });
+    });
+
+});
