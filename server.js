4dd2b82a70e4ef5421893d3b937bbcf57eee2132
Wendy Beck <webeck@gmail.com>
2017-08-07 12:29:42 -0700

    fixing database helper

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 /* eslint no-console: "off" */
+require('dotenv').config();
 const app = require('./lib/app');
 const http = require('http');
-require('dotenv').config()
-const dbUri = process.env.MONGO_URI;
-const connect = require('./lib/connect');
+require('./lib/connect');
 
-connect(dbUri);
 const server = http.createServer(app);
 
 server.listen(3001, () => {
