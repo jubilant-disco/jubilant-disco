559dba4c4a22411627bfa480454d04051e84f5ee
Anwar Montasir <anwarmontasir@gmail.com>
2017-08-08 11:32:35 -0700

    14 passing auth tests

--- tests/e2e/auth.test.js
+++ tests/e2e/auth.test.js
@@ -47,7 +47,7 @@ describe('auth', () => {
         it('signup', () => {
             return request.post('/auth/signup')
                 .send(user)
-                .then(res => assert.ok(res.body.token));
+                .then(res => assert.ok(token = res.body.token));
         });
 
         it('cant use the same email', () =>
