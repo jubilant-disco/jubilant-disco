28a6382f3858949483826137215cf138678625de
Anwar Montasir <anwarmontasir@gmail.com>
2017-08-08 11:28:48 -0700

    13 passing auth tests

--- tests/unit/user.test.js
+++ tests/unit/user.test.js
@@ -2,11 +2,11 @@ const User = require('../../lib/models/user');
 const { assert } = require('chai');
 
 describe('user model unit test', () => {
-    it.skip('validates the user', () => {
+    it('validates the user', () => {
         const user = new User({
             name: 'Andrew',
             email: 'andrew@andrew.com',
-            password: 'blhahasd',
+            hash: 'blhahasd',
             favAlbums: [{
                 title: 'Kenny Loggins Greatest Hits',
                 artist: 'Kenny Loggin',
@@ -17,14 +17,14 @@ describe('user model unit test', () => {
         return user.validate();
     });
 
-    it.skip('fails validation if required fields missing', () => {
+    it('fails validation if required fields missing', () => {
         const user = new User();
         return user.validate()
             .then(() => { throw new Error('expected validation error');},
                 ({ errors }) => {
                     assert.ok(errors.name);
                     assert.ok(errors.email);
-                    assert.ok(errors.password);
+                    assert.ok(errors.hash);
                 });
     });
 });
