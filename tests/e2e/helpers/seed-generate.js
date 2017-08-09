cd2bfbdc32dd92385316fbe84bf440481776947c
Wendy Beck <webeck@gmail.com>
2017-08-09 02:53:05 -0700

    finished album seed

--- tests/e2e/helpers/seed-generate.js
+++ tests/e2e/helpers/seed-generate.js
@@ -1,29 +1,37 @@
+require('dotenv')
+    .config();
 const req = require('./request');
-const albums = require('../../../discogs-data/100albums');
+const albums = require('../../../discogs-data/120albums');
+const db = require('./db');
 
-for (let i = 0; i < 1; i++) {
-    let masterId = albums.items[i].id;
-    buildAlbumData(masterId)
-        .then(res => console.log(res))
-}
 
-function buildAlbumData(masterId) {
-    return req.get(`/seeds/${masterId}`)
-        .set('Authorization', {key:'fCoUCDJwscUNWfhQPUxS', secret: 'UlurrruRvEhpdUGbyijsjwzyxBMMcKmX'})
-        .then(res => {
-            const albumObj = {
-                title: res.body.title,
-                artist: res.body.artists[0].name,
-                genre: res.body.genres,
-                discogsMasterId: masterId,
-                discogMasterUrl: res.body.resource_url
-            }
-            return albumObj
-        })
-        .then(obj => {
-            console.log('afterget: ',obj)
-            return req.post('/seeds')
-                .send(obj)
-                .then(res => {})
-        })
+let i = 0;
+db.drop();
+
+setInterval(buildAlbumData, 8000);
+
+
+function buildAlbumData() {
+    if (i <= albums.items.length) {
+
+        let masterId = albums.items[i].id;
+        i++;
+        return req.get(`/seeds/${masterId}`)
+            .set('Authorization', { key: `${process.env.DISCOGS_CONSUMER_KEY}`, secret: `${process.env.DISCOGS_CONSUMER_SECRET}` })
+            .then(res => {
+                const albumObj = {
+                    title: res.body.title,
+                    artist: res.body.artists[0].name,
+                    genre: res.body.genres,
+                    discogsMasterId: masterId,
+                    discogMasterUrl: res.body.resource_url
+                }
+                return albumObj
+            })
+            .then(obj => {
+                return req.post('/seeds')
+                    .send(obj)
+                    .then(res => console.log(res.status))
+            })
+    }
 }
