422a4cabc7f1481061b8886836c30936cde02649
Wendy Beck <webeck@gmail.com>
2017-08-09 10:56:34 -0700

    starting match tests

---
+++ tests/e2e/math.test.js
@@ -0,0 +1,22 @@
+const request = require('./helpers/request');
+const { assert } = require('chai');
+
+describe('build user data', () => {
+
+    // beforeEach(db.drop);
+
+
+    it('verify album data has length', () => {
+
+    })
+    it('verify user data has length', () => {
+
+    })
+    it('retrieve user and verify they have albums', () => {
+
+    })
+    it('retrieve user and create dictionary objects', () => {
+
+    })
+
+})
