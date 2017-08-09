c51dedaa0ecc39d98cb1e4978a26d21b4ebc4407
Wendy Beck <webeck@gmail.com>
2017-08-09 04:49:19 -0700

    the discogs api seems to have gone down

--- tests/e2e/helpers/seed-generate.js
+++ tests/e2e/helpers/seed-generate.js
@@ -2,23 +2,23 @@ require('dotenv')
     .config();
 const req = require('./request');
 const albums = require('../../../discogs-data/120albums');
+const seedUsers = require('../../../discogs-data/120userNames');
 const db = require('./db');
-
 
 let i = 0;
 db.drop();
 
-setInterval(buildAlbumData, 8000);
-
+setInterval(buildAlbumData, 10000);
 
 function buildAlbumData() {
-    if (i <= albums.items.length) {
-
+    if (i <= 120) {
         let masterId = albums.items[i].id;
         i++;
         return req.get(`/seeds/${masterId}`)
             .set('Authorization', { key: `${process.env.DISCOGS_CONSUMER_KEY}`, secret: `${process.env.DISCOGS_CONSUMER_SECRET}` })
+            .send({id: masterId})
             .then(res => {
+                console.log(res);
                 const albumObj = {
                     title: res.body.title,
                     artist: res.body.artists[0].name,
@@ -29,9 +29,30 @@ function buildAlbumData() {
                 return albumObj
             })
             .then(obj => {
-                return req.post('/seeds')
+                console.log(obj);
+                return req.post('/seeds/albums')
                     .send(obj)
                     .then(res => console.log(res.status))
             })
     }
+}
+
+
+
+function buildUserData(j) {
+    for (let j = 0; j <= 120; j++) {
+
+        let userSeed = seedUsers.users[j];
+        return req.get('/seeds/random')
+            .then(res => {
+                userSeed.favAlbums = res.body;
+                return userSeed;
+            })
+            .then(user => {
+                return req.post('/seeds/users')
+                    .send({ user })
+                    .then(res => console.log(res.status))
+            })
+    }
+
 }
