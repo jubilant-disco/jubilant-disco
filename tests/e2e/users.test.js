2a68247db018deb83c74f7b3382fc18130a4fca0
Meryl Turner <merylturner08@gmail.com>
2017-08-09 09:34:16 -0700

    removed console.log

--- tests/e2e/users.test.js
+++ tests/e2e/users.test.js
@@ -118,7 +118,6 @@ describe('user routes', () => {
     });
 
     it('removes an album from user favAlbums array', () => {
-        console.log('WHAT IS JOE', joe);
         return request.delete(`/users/${joe._id}/albums/${joe.favAlbums[1]._id}`)
             .then(res => res.body)
             .then(result => {
